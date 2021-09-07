83c568be7dc6874589c1570cb285e16c8164353d
Patrycja <pati.iwanek@gmail.com>
2021-09-07 21:49:09 +0200

    basic script for starting project taken from w3school

--- main.py
+++ main.py
@@ -6,14 +6,13 @@ from datetime import datetime
 from flask import flash, redirect, url_for, request
 
  # https://www.educative.io/blog/javascript-snake-game-tutorial
- 
+# https://www.codeexplained.org/2018/08/create-snake-game-using-javascript.html
+# https://www.educative.io/blog/javascript-snake-game-tutorial
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    # return 'Cześć, tu Python!'
-    return render_template('index.html')
-
+    return render_template('test.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
